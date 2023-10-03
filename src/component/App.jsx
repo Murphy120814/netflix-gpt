10937a5487c31ad0b6ccb95d9ff95f5d0ad6977c
Murphy <109974838+Murphy120814@users.noreply.github.com>
2023-10-03 15:58:00 +0530

    initial commit
    CRA and Tailwind setup complete

---
+++ src/component/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold p-4  ">
+      This is app which is called as netflix gpt
+    </div>
+  );
+}
+
+export default App;
